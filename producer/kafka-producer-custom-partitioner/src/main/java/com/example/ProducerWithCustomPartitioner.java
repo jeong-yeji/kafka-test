e3a9c5121ff0e0609abff0f4e7a9cdef00c6dd90
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-26 16:51:25 +0900

    feat: 커스텀 파티셔너 프로듀서 애플리케이션

---
+++ producer/kafka-producer-custom-partitioner/src/main/java/com/example/ProducerWithCustomPartitioner.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerWithCustomPartitioner {
+
+    private final static Logger logger = LoggerFactory.getLogger(ProducerWithCustomPartitioner.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Suwon", "Suwon");
+        producer.send(record);
+
+        logger.info("{}", record);
+
+        producer.flush();
+        producer.close();
+    }
+}
