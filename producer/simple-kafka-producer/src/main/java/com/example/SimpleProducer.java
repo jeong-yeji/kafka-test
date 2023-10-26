526e06c3e458c9659504131d8355e6b55c13d879
jeong-yeji <jeongyeji.mail@gmail.com>
2023-10-26 16:49:17 +0900

    feat: 프로듀서 애플리케이션 개발하기

---
+++ producer/simple-kafka-producer/src/main/java/com/example/SimpleProducer.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProducer {
+
+    private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+    private final static String MESSAGE_VALUE = "testMessage";
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, MESSAGE_VALUE);
+        producer.send(record);
+
+        logger.info("{}", record);
+
+        producer.flush(); // Accumulator에 있는 모든 데이터 강제 전송
+        producer.close(); // Producer를 안전하게 종료
+    }
+}
